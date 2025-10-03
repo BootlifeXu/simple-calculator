cac4b3c92ab30dd8b3dfcb55e9ced486a7aa83a5
Samir Gaylord <129090835+BootlifeXu@users.noreply.github.com>
2025-10-03 16:03:19 +0800

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,16 +5,19 @@ allprojects {
     }
 }
 
+// Custom build directory inside the project (safe from permission issues)
 val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
+    rootProject.layout.projectDirectory
+        .dir("custom_build")
         .get()
+
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
